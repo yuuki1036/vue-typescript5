980a3b4ca67da0c7657bb097e35e8e38f837d223
yuuki1036 <yuuki1036@gmail.com>
2022-05-09 09:11:00 +0900

    custom directive

---
+++ src/components/Rating.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+// カスタムv-model
+const props = defineProps(['modelValue'])
+const emits = defineEmits(['update:modelValue'])
+
+const isActiveButton = ref<string>(props.modelValue)
+
+const onClick = (buttonType: string) => {
+  isActiveButton.value = buttonType
+  emits('update:modelValue', isActiveButton.value)
+}
+</script>
+
+<template>
+<ul>
+  <li>
+    <button type="button" :class="{active: isActiveButton === 'bad'}" @click="onClick('bad')">Bad</button>
+  </li>
+  <li>
+    <button type="button" :class="{active: isActiveButton === 'ave'}" @click="onClick('ave')">Average</button>
+  </li>
+  <li>
+    <button type="button" :class="{active: isActiveButton === 'grt'}" @click="onClick('grt')">Great</button>
+  </li>
+</ul>
+</template>
+
+<style scoped>
+.active {
+  color: red;
+}
+ul {
+  list-style: none;
+  margin: .5rem 0;
+  padding: 0;
+  display: flex;
+}
+li {
+  margin: 0 1rem;
+  border: 1px solid gray;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
